c05416c499b9741b524a7349b6fab71a26dbf335
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2024-10-08 13:03:45 -0300

    Commit inicial
    Inicialização do projeto

---
+++ src/main/java/com/wearit/wearit_backend/WearitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wearit.wearit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wearit/wearit_backend/WearitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wearit.wearit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
