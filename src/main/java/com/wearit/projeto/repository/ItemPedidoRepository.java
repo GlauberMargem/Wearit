b09cd58f73a9d572b84fd64de9dd539cf31cccec
jjeancarlos <jeanpastebin@gmail.com>
2024-10-10 20:52:40 -0300

    feat:Uma nova funcionalidade

---
+++ src/main/java/com/wearit/projeto/repository/ItemPedidoRepository.java
@@ -0,0 +1,8 @@
+package com.wearit.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.wearit.projeto.entity.ItemPedidoEntity;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedidoEntity, Long> {
+
+}
